089b322db811c39a38e8372766f4301f3c0c714c
EkaterinaFefelova <fefelova.ekaterina@gmail.com>
2024-07-25 20:13:19 +0300

    HW15 completed

--- Task2/src/Main.java
+++ Task2/src/Main.java
@@ -1,5 +1,48 @@
+import java.util.Scanner;
+
 public class Main {
+
+    public static final String VALID_STRING = " -АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+
+
     public static void main(String[] args) {
-        //todo код писать тут. По желанию(рекомендуется) вынести код в методы.
+    String input = getInput();
+    if(validateInput(input))
+        printFIO(input);
+    }
+
+    public static String getInput(){
+        System.out.println("Пожалуйста, введите свои фамилию, имя и отчество, разделенные пробелами: ");
+        return new Scanner(System.in).nextLine().trim();
+    }
+
+    public static boolean validateInput (String input){
+        boolean isValid = true;
+
+        for (int i=0; i<input.length(); i++) {
+            if (!VALID_STRING.contains(String.valueOf(input.charAt(i)))) {
+                isValid = false;
+                break;
+            }
+        }
+
+        int spaces = 0;
+        for (int i=0; i<input.length(); i++){
+            if (input.charAt(i)== ' ')
+                spaces++;
+        }
+        if (spaces!=2)
+            isValid = false;
+
+        if (!isValid)
+            System.out.println("Введенная строка не является ФИО!");
+
+        return isValid;
+    }
+
+    public static void printFIO(String input){
+        System.out.println("Фамилия: " + input.substring(0, input.indexOf(' ')));
+        System.out.println("Имя: " + input.substring(input.indexOf(' '), input.lastIndexOf(' ')).trim());
+        System.out.println("Отчество: " + input.substring(input.lastIndexOf(' ')).trim());
     }
 }
